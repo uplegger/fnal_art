78296111630c40a17e0f56b39e88773f029a6ef0
Eric Flumerfelt <eflumerf@fnal.gov>
2023-08-18 15:10:36 -0500

    Set CET_PLUGIN_PATH and FHICL_FILE_PATH for artdaq packages

--- packages/otsdaq-mu2e/package.py
+++ packages/otsdaq-mu2e/package.py
@@ -10,6 +10,12 @@ import sys
 from spack import *
 
 
+def sanitize_environments(env, *vars):
+    for var in vars:
+        env.prune_duplicate_paths(var)
+        env.deprioritize_system_paths(var)
+
+
 class OtsdaqMu2e(CMakePackage):
     """The toolkit currently provides functionality for data transfer,
     event building, event reconstruction and analysis (using the art analysis
@@ -44,4 +50,22 @@ class OtsdaqMu2e(CMakePackage):
     depends_on("otsdaq-components")
     depends_on("otsdaq-epics")
     depends_on("artdaq-mu2e")
+
+    def setup_run_environment(self, env):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
+
+    def setup_dependent_run_environment(self, env, dependent_spec):
+        prefix = self.prefix
+        # Ensure we can find plugin libraries.
+        env.prepend_path("CET_PLUGIN_PATH", prefix.lib)
+        # Ensure we can find fhicl files
+        env.prepend_path("FHICL_FILE_PATH", prefix + "/fcl")
+        # Cleaup.
+        sanitize_environments(env, "CET_PLUGIN_PATH", "FHICL_FILE_PATH")
 
