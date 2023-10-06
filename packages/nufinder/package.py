3c2c4c449476a7e36b6565bd1b653c96b9740b7b
Marc Mengel <mengel@fnal.gov>
2023-10-06 14:15:44 -0500

    adding nufinder

---
+++ packages/nufinder/package.py
@@ -0,0 +1,17 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+class Nufinder(CMakePackage):
+    """CMake package finding macros for nutools suite"""
+
+    homepage = "https://github.com/NuSoftHEP/nufinder"
+    url = "https://github.com/NuSoftHEP/nufinder/archive/refs/tags/v1_01_02.tar.gz"
+
+    maintainers(("marcmengel", "nusense"))
+
+    version("1_01_02", sha256="07cb659967012399bbde27ac5cdf4188d71edfb685d7b6f6bc1d24ff4a7bd987")
+
