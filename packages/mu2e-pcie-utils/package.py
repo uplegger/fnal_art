ed724b62d056cba9e0b7f089a73c0fc6d23e504e
Patrick Gartung <gartung@fnal.gov>
2023-10-31 19:28:07 -0500

    Use sha256 instead of git commit

--- packages/mu2e-pcie-utils/package.py
+++ packages/mu2e-pcie-utils/package.py
@@ -22,8 +22,8 @@ class Mu2ePcieUtils(CMakePackage):
     git = "https://github.com/Mu2e/mu2e_pcie_utils.git"
 
     version("develop", branch="develop", get_full_repo=True)
-    version("v2_08_05", commit="adf1cbc")
-    version("v2_08_00", commit="32899a1")
+    version("v2_08_05", sha256="2e78e63c9f71b0293b6e09315a17f809b42a40d5bc1e7b2cc4efc8f3b042a79b")
+    version("v2_08_00", sha256="e322ff8f4fbc2c8aaf59ce1afcc5ac410b459717df18ba5432ea9f96be609083")
 
     def url_for_version(self, version):
         url = "https://github.com/Mu2e/mu2e_pcie_utils/archive/refs/tags/{0}.tar.gz"
