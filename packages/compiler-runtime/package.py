43d90356e7a18d879a0d51a21022766db95bcf23
Patrick Gartung <gartung@fnal.gov>
2023-11-17 13:20:27 -0600

    Use implementation from PR 41104, rename to CompilerRuntime

--- packages/compiler-runtime/package.py
+++ packages/compiler-runtime/package.py
@@ -3,47 +3,67 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import os
+import re
 
 from spack.package import *
-import os
+from spack.util.elf import parse_elf
 
 
 class CompilerRuntime(Package):
-    """FIXME: Put a proper description of your package here."""
+    """Package for compiler runtime libraries"""
 
-    homepage = "https://github.com/FNALssi/"
-    url = "file:///dev/null"
+    homepage = "https://gcc.gnu.org"
+    has_code = False
 
-    maintainers("marcmengel")
+    maintainers("haampie")
 
-    version("see_compiler", sha256="364fd6d448b2398150a4d8aa3a3d3564a373aeb7dfb3a477f56685a356c88159")
+    license("GPL-3.0-only")
 
-    def url_for_version(self, version):
+    version("1.0")
 
-        if not os.access("/tmp/empty.tar",os.R_OK):
-            tdir = "/tmp/empty" + str(os.getpid())
-            os.mkdir(tdir)
-            tar=which("tar")
-            with working_dir(tdir):
-                # --mtime=@0 gives consistent tarfile checksum...
-                tar("cf", "/tmp/empty.tar", ".", "--mtime=@0")
-            os.rmdir(tdir)
-
-        url = 'file:///tmp/empty.tar'
-        return url
-
+    requires("%gcc", "%clang")
 
     def install(self, spec, prefix):
-         libdir=''
-         cdir = os.path.dirname(os.path.dirname(self.compiler.cxx))
-         # find shortest pathname in cdir that has libstdc++.so in it...
-         for path, dirnames, filenames in os.walk(cdir):
-             if 'libstdc++.so' in filenames:
-                 if not libdir or len(path) < len(libdir):
-                     libdir = path
-         if libdir:
-             os.makedirs(prefix.lib)
-             install_tree(libdir, prefix.lib)
+        cc = Executable(self.compiler.cc)
+        lib_regex = re.compile(rb"\blib[a-z-_]+\.so\.\d+\b")
 
-    def setup_dependent_runtime_environment(self, env):
-        env.append_flags("LDFLAGS", "-L"+self.prefix.lib)
+        mkdir(prefix.lib)
+
+        for name in ["atomic", "gcc_s", "gfortran", "gomp", "stdc++", "quadmath"]:
+            # Look for the dynamic library that gcc would use to link,
+            # that is with .so extension and without abi suffix.
+            path = cc(f"--print-file-name=lib{name}.so", output=str).strip()
+
+            # gcc reports an absolute path on success
+            if not os.path.isabs(path):
+                continue
+
+            # Now there are two options:
+            # 1. the file is an ELF file
+            # 2. the file is a linker script referencing the actual library
+            with open(path, "rb") as f:
+                try:
+                    # Try to parse as an ELF file
+                    soname = parse_elf(f, dynamic_section=True).dt_soname_str.decode("utf-8")
+                except Exception:
+                    # On failure try to "parse" as ld script; the actual
+                    # library needs to be mentioned by filename.
+                    f.seek(0)
+                    script_matches = lib_regex.findall(f.read())
+                    if len(script_matches) != 1:
+                        continue
+                    soname = script_matches[0].decode("utf-8")
+
+            # Now locate and install the runtime library
+            runtime_path = cc(f"--print-file-name={soname}", output=str).strip()
+
+            if not os.path.isabs(runtime_path):
+                continue
+
+            install(runtime_path, os.path.join(prefix.lib, soname))
+
+    @property
+    def libs(self):
+        # Currently these libs are not linkable with -l, they all have a suffix.
+        return LibraryList([])
