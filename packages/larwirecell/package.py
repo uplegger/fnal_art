f2d91600f0267140589962823bd10fed237695aa
Patrick Gartung <gartung@fnal.gov>
2023-11-27 18:26:21 -0600

    larwirecell: escape regex special characters

--- packages/larwirecell/package.py
+++ packages/larwirecell/package.py
@@ -110,11 +110,11 @@ class Larwirecell(CMakePackage):
             'Modules/FindWireCell.cmake'
         )
         filter_file(
-            r'set(_fwc_fphsa_extra_args',
-            'STRING(REPLACE ";" " " _fwc_missing_deps_str "missing dependencies: ${_fwc_missing_deps}")\n    set(_fwc_fphsa_extra_args',
+            r' set\(_fwc_fphsa_extra_args',
+             ' STRING(REPLACE ";" " " _fwc_missing_deps_str "missing dependencies: ${_fwc_missing_deps}")\n    set(_fwc_fphsa_extra_args',
             'Modules/FindWireCell.cmake')
         filter_file(
-            r'REASON_FAILURE_MESSAGE "missing dependencies: ${_fwc_missing_deps}"',
+            r'REASON_FAILURE_MESSAGE "missing dependencies: \$\{_fwc_missing_deps\}"',
             'REASON_FAILURE_MESSAGE "missing dependencies: ${_fwc_missing_deps_str}"',
             'Modules/FindWireCell.cmake'
         )
