8e90480114edd6852f45349b1d3cd720bdca9a6d
Chris Green <greenc@fnal.gov>
2024-02-22 11:20:16 -0600

    [art] Boost version dependence should be a semi-unbounded range

--- packages/art/package.py
+++ packages/art/package.py
@@ -49,7 +49,7 @@ class Art(CMakePackage):
     conflicts("cxxstd=17", when="@develop")
 
     depends_on("boost+date_time+graph+program_options+regex")
-    depends_on("boost@1.75 +filesystem+json+test+thread", type=("build"))
+    depends_on("boost@1.75: +filesystem+json+test+thread", type=("build"))
     depends_on("boost+graph+test", type=("test"))
     depends_on("canvas")
     depends_on("catch2@2.3.0", type=("build", "test"), when="@:3.11.99")
